b2922bf89f90289de9342b42c13b18bc90306c13
M4deN <alexdesaran@outlook.com>
2025-11-11 21:09:57 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/lista_leitura_react/',  
-  plugins: [react()],
+  plugins: [react()],  
 })
